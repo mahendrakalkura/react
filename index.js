ec96515e71472fa81b965a0a3803e917ca83f7f1
Mahendra Kalkura <mahendra@kalkura.com>
2016-03-23 18:39:49 +0530

    index.js

--- index.js
+++ index.js
@@ -4,35 +4,66 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Item = React.createClass({
+    getInitialState: function() {
+        return {
+            key: this.props.key,
+            value: this.props.value
+        };
+    },
+    onClick: function() {
+        this.setState({
+            value: new Date().getTime()
+        });
+    },
     render: function() {
         return (
             <div className="item">
-                <h1>{this.props.value}</h1>
-                <p>{this.props.value}</p>
+                <h1 onClick={this.onClick}>{this.state.value}</h1>
+                <p>{this.state.value}</p>
             </div>
         );
     }
 });
 
 var Items = React.createClass({
+    getInitialState: function() {
+        return {
+            items: this.props.items
+        };
+    },
+    onClick: function() {
+        this.state.items.push({
+            key: new Date().getTime(),
+            value: new Date().getTime()
+        });
+        this.setState({
+            items: this.state.items
+        });
+    },
     render: function() {
-        var items = this.props.items.map(function(item) {
+        var onClick = this.onClick;
+        var items = this.state.items.map(function(item) {
             return (
                 <Item key={item.key} value={item.value}></Item>
             );
         });
         return (
-            <div className="items">{items}</div>
+            <div className="items">
+                <p>
+                    <a className="btn btn-success" onClick={this.onClick}>Add</a>
+                </p>
+                {items}
+            </div>
         );
     }
 });
 
 var items = [];
-for (var index = 0; index <= 999; index = index + 1) {
+for (var index = 1; index <= 9999; index = index + 1) {
     items.push({
         key: index,
         value: index
-    });
+    })
 }
 
 ReactDOM.render(
