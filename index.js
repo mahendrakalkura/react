f79f52258e7fcc64c02baf51845b1eb77a356c83
Mahendra Kalkura <mahendra@kalkura.com>
2016-03-23 16:25:49 +0530

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,39 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Item = React.createClass({
+    render: function() {
+        return (
+            <div className="item">
+                <h1>{this.props.value}</h1>
+                <p>{this.props.value}</p>
+            </div>
+        );
+    }
+});
+
+var Items = React.createClass({
+    render: function() {
+        var items = this.props.items.map(function(item) {
+            return (
+                <Item key={item.key} value={item.value}></Item>
+            );
+        });
+        return (
+            <div className="items">{items}</div>
+        );
+    }
+});
+
+var items = [];
+for (var index = 0; index <= 999; index = index + 1) {
+    items.push({
+        key: index,
+        value: index
+    });
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <Items items={items}/>,
     document.getElementById('div')
 );
